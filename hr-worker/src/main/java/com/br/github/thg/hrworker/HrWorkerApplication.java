58edc9cf473ca44ab9d3093a33fa371bceff9b0a
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-13 11:12:38 -0300

    Project hr-worker created

---
+++ hr-worker/src/main/java/com/br/github/thg/hrworker/HrWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.br.github.thg.hrworker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrWorkerApplication.class, args);
+	}
+
+}
