f7c234a112782a955154765ef7692e9a3521f6cc
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-13 17:17:55 -0300

    project hr-payroll created

---
+++ hr-payroll/src/test/java/com/github/thg/hrpayroll/HrPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thg.hrpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
