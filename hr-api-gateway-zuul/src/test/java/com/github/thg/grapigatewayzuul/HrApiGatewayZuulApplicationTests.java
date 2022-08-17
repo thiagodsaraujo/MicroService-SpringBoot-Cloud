0b3012b06192f30fb1f578ddd44ac4c80e551a98
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-17 14:48:27 -0300

    Configuring Zuul api gateway

--- hr-api-gateway-zuul/src/test/java/com/github/thg/grapigatewayzuul/HrApiGatewayZuulApplicationTests.java
+++ hr-api-gateway-zuul/src/test/java/com/github/thg/grapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HrEurekaServer1ApplicationTests {
+class HrApiGatewayZuulApplicationTests {
 
 	@Test
 	void contextLoads() {
