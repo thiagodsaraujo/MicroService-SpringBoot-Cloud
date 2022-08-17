20bc0f1fdd4500278430a64116d9ff723faa871a
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-17 14:24:06 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/main/java/com/github/thg/grapigatewayzuul/HrEurekaServer1Application.java
@@ -0,0 +1,13 @@
+package com.github.thg.grapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrEurekaServer1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrEurekaServer1Application.class, args);
+	}
+
+}
---
+++ hr-api-gateway-zuul/src/test/java/com/github/thg/grapigatewayzuul/HrEurekaServer1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thg.grapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServer1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
