a4db32e24e0cf1196d131184f33cb58abcc267e9
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-22 19:35:47 -0300

    Database seeding

---
+++ hr-user/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO tb_user (name, email, password) VALUES ('Nina Brown', 'nina@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Leia Red', 'leia@gmail.com', '$2a$10$NYFZ/8WaQ3Qb6FCs.00jce4nxX9w7AkgWVsQCG6oUwTAcZqP9Flqu');
+INSERT INTO tb_user (name, email, password) VALUES ('Tita', 'tita@gmail.com', '$2a$10$L513p/ijMh35SUDm34nxwutrsrOmHjnPclaHf7BeY1bhHnwqKIP8G');
+
+INSERT INTO tb_role (role_name) VALUES ('ROLE_OPERATOR');
+INSERT INTO tb_role (role_name) VALUES ('ROLE_ADMIN');
+
+INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
