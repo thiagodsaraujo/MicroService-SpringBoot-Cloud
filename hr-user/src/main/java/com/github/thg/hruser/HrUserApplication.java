aeeee39f0fa9f5f7ad3fa887f632686c8e6b8f0b
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-22 18:56:56 -0300

    config hr-user project

--- hr-user/src/main/java/com/github/thg/hruser/HrUserApplication.java
+++ hr-user/src/main/java/com/github/thg/hruser/HrUserApplication.java
@@ -2,7 +2,9 @@ package com.github.thg.hruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HrUserApplication {
 
