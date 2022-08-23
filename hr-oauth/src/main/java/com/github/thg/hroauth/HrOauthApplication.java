5fec4432fc31080f3095ca9253a3571e4df55d61
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-08-23 15:51:08 -0300

    Configuring hr-oauth project

--- hr-oauth/src/main/java/com/github/thg/hroauth/HrOauthApplication.java
+++ hr-oauth/src/main/java/com/github/thg/hroauth/HrOauthApplication.java
@@ -2,7 +2,10 @@ package com.github.thg.hroauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class HrOauthApplication {
 
