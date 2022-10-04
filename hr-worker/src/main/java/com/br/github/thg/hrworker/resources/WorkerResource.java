c0ab27caf03af58a82f478d42b91413ae77f9e3a
user.email <thiago.dos.araujo@maisunifacisa.com.br>
2022-10-04 14:34:10 -0300

    Docker start

--- hr-worker/src/main/java/com/br/github/thg/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/br/github/thg/hrworker/resources/WorkerResource.java
@@ -1,7 +1,11 @@
 package com.br.github.thg.hrworker.resources;
 
+import java.util.HashMap;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import org.hibernate.mapping.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +13,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -38,6 +46,13 @@ public class WorkerResource {
 		logger.info("CONFIG: " + testeConfig);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@PostMapping(value = "/createWorker")
+	public ResponseEntity<Worker> createWorker(@Valid @RequestBody Worker worker){
+		final Worker updateWorker = repository.save(worker);
+		return ResponseEntity.ok(updateWorker);
+		
+	}
 
 	
 	@GetMapping
@@ -63,5 +78,31 @@ public class WorkerResource {
 		Worker obj = repository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
+
+	
+	@PutMapping(value = "/updateWorker/{id}")
+	public ResponseEntity<Worker> updateWorkerById(@PathVariable Long id, @RequestBody Worker workerDetails)
+	throws Exception {
+		Worker worker = repository.findById(id)
+				.orElseThrow(() -> new Exception("Worker not found for this id :" + id));
+		
+		worker.setId(workerDetails.getId());
+		worker.setName(workerDetails.getName());
+		worker.setDailyIncome(workerDetails.getDailyIncome());
+		
+		final Worker updateWorker = repository.save(worker);
+		
+		return ResponseEntity.ok(updateWorker);
+
+	}
+	
+//	@DeleteMapping(value = "/deleteWorker/{id}")
+//	public ResponseEntity<Worker> deleteWorker(@PathVariable(value = "id") Long id) throws Exception {
+//		Worker worker = repository.findById(id)
+//				.orElseThrow(() -> new Exception("Worker not found for this id :" + id));
+//		final Worker deletedWorker = repository.deleteById(id);
+//		
+//	}
+	
 	
 }
